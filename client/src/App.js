1d55e64dba292e1a1ff0e81f2e4dacdf1cb382e7
Saurabh Burade <sburade3@gmail.com>
2020-08-31 14:43:25 +0530

    add socketio

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from "socket.io-client";
+
 import './App.css';
+const ENDPOINT = "http://127.0.0.1:8000";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    const [response, setResponse] = useState("");
+
+    useEffect(() => {
+        const socket = socketIOClient(ENDPOINT);
+        socket.on("changeData", data => {
+            setResponse(data);
+            console.log(data)
+        });
+    }, []);
+
+    return (
+        <p>
+            It's
+        </p>
+    );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -5,20 +5,53 @@ const PORT = process.env.PORT || 8000;
 const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const User=require('./models/users.model');
 const {uri, dbName} = require("./configs/db.config");
 console.log(uri,dbName)
-mongoose.connect(uri, {useUnifiedTopology: true, dbName}, err => {
-    if (err) {
-        console.log("Mongoose conection error", err);
-    } else {
-        console.log("Mongoose conection : connected 🔥");
+
+mongoose.connect(
+    uri,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        dbName,
+        replicaSet: "rs0",
+    },
+    err => {
+        if (err) {
+            console.log("Mongoose conection error", err);
+        } else {
+            console.log("Mongoose conection : connected 🔥");
+        }
     }
+);
+const changeStream = User.watch();
+console.log(changeStream)
+changeStream.on("change", change => {
+    console.log("change",change); 
+    io.emit('changeData',change)
 });
+io.on("connection", function () {
+    console.log("connected");
+});
+// const newUser=new User({
+//     fname:"john"
+// }) ;
+// newUser.save((err,doc)=>{ 
+//     console.log(err,doc);
+// })
 app.use(express.json());
 app.use(cors());
 app.use(morgan());
-
-app.listen(PORT, err => {
+app.get("/",(req,res)=>{
+    User.find((err,docs)=>{
+        io.on("changeData",change=>console.log(change));
+        res.send(docs)
+    })
+})
+http.listen(PORT, err => {
     if (err) throw err;
     else console.log("Connected to PORT : ", PORT);
 });
