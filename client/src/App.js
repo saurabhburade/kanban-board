ddf88f6c38106b529647982b41d9517421144d3d
Saurabh Burade <sburade3@gmail.com>
2020-09-05 10:07:14 +0530

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import socketIOClient from "socket.io-client";
-
+import Header from "./Components/Header/Header";
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/SignUp/SignUp";
+import Home from "./Components/home/Home";
 import './App.css';
 const ENDPOINT = "http://127.0.0.1:8000";
 
@@ -15,11 +19,18 @@ function App() {
         });
     }, []);
 
-    return (
-        <p>
-            It's
-        </p>
-    );
+       return (
+           <Router>
+               <div className="App">
+                   <Header />
+                   <Switch>
+                       <Route path="/login" exact component={Login} />
+                       <Route path="/" exact component={Home} />
+                       <Route path="/register" exact component={SignUp} />
+                   </Switch>
+               </div>
+           </Router>
+       );
 }
 
 export default App;
