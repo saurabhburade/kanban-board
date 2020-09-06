3fe9fd4a0ccaf1f5e804ac95f81f9ab7d91e650b
Saurabh Burade <sburade3@gmail.com>
2020-09-06 18:42:37 +0530

    Add SocketIO New Board Events

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from "react";
 import socketIOClient from "socket.io-client";
 import Header from "./Components/Header/Header";
 import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/SignUp/SignUp";
 import Home from "./Components/home/Home";
-import './App.css';
-import Profile from './Components/Profile/Profile';
-import { fetchUser } from './Redux/user/user.actionCreators';
-import { connect } from 'react-redux';
+import "./App.css";
+import Profile from "./Components/Profile/Profile";
+import {fetchUser} from "./Redux/user/user.actionCreators";
+import {connect} from "react-redux";
+import {isAuth} from "./Utils/auth";
 const ENDPOINT = "http://127.0.0.1:8000";
 
 function App(props) {
@@ -16,31 +17,35 @@ function App(props) {
 
     useEffect(() => {
         const socket = socketIOClient(ENDPOINT);
+        socket.emit("connected", {key: localStorage.getItem("_id")});
         socket.on("changeData", data => {
             setResponse(data);
-            console.log(data)
+            if (!!data) {
+                props.fetchUser();
+            }
+            console.log(data);
         });
     }, []);
-useEffect(() => {
-    props.fetchUser()
-}, [])
-       return (
-           <Router>
-               <div className="App">
-                   <Header />
-                   <Switch>
-                       <Route path="/login" exact component={Login} />
-                       <Route path="/" exact component={Home} />
-                       <Route path="/register" exact component={SignUp} />
-                       <Route path="/dashboard" exact component={Profile} />
-                   </Switch>
-               </div>
-           </Router>
-       );
+    useEffect(() => {
+        props.fetchUser();
+    }, []);
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                <Switch>
+                    <Route path="/login" exact component={Login} />
+                    <Route path="/" exact component={Home} />
+                    <Route path="/register" exact component={SignUp} />
+                    <Route path="/dashboard" exact component={Profile} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-const mapDispatchToProps =dispatch=> {
-   return { fetchUser:()=>dispatch(fetchUser())}
-}
+const mapDispatchToProps = dispatch => {
+    return {fetchUser: () => dispatch(fetchUser())};
+};
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(null, mapDispatchToProps)(App);
