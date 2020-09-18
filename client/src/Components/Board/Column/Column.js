ba4f571c0afb6493536687a87954dd3e1711b5e2
Saurabh Burade <sburade3@gmail.com>
2020-09-18 22:25:57 +0530

    add - task modal

--- client/src/Components/Board/Column/Column.js
+++ client/src/Components/Board/Column/Column.js
@@ -7,9 +7,16 @@ import {FileAddOutlined} from "@ant-design/icons";
 
 import {connect} from "react-redux";
 import {updateOnTaskMove} from "./../../../Utils/boardHelpers";
+import AddBoardModal from "./../../AddBoard/AddBoardModal";
+import AddTaskModal from './../TaskCard/AddTaskModal';
 
 function Column({board, _id}) {
     const [columns, setColumns] = useState(board?.columns);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [activeColumnName, setactiveColumnName] = useState("")
+    const handleModalCancel = () => {
+        setModalVisible(false);
+    };
     useEffect(() => {
         setColumns(board?.columns);
     }, [board]);
@@ -88,92 +95,111 @@ function Column({board, _id}) {
         }
     };
     return (
-        <DragDropContext onDragEnd={dragEnd}>
-            {columns?.map((columnItem, i) => {
-                return (
-                    <div className="column-main">
-                        <div className="col-head w-100 d-flex justify-content-between pl-3 pr-3   ">
-                            <h5>{columnItem.columnName}</h5>
-                            <Tooltip title="Add task">
-                                <Button
-                                    icon={<FileAddOutlined />}
-                                    shape="circle"
-                                />
-                            </Tooltip>
-                        </div>
-                        <Droppable droppableId={columnItem.columnName} key={i}>
-                            {(provided, snapshot) => {
-                                return (
-                                    <div
-                                        className="colmn-cont-secondary"
-                                        ref={provided.innerRef}
-                                        {...provided.droppableProps}
-                                        style={{
-                                            minHeight: "100px",
-
-                                            background: snapshot.isDraggingOver
-                                                ? "lightblue"
-                                                : "transparent",
+        <>
+          
+            <DragDropContext onDragEnd={dragEnd}>
+                {columns?.map((columnItem, i) => {
+                    return (
+                        <div className="column-main">
+                            <AddTaskModal
+                                modalVisible={modalVisible}
+                                onCancel={handleModalCancel}
+                                columnName={activeColumnName}
+                                key={i}
+                                _id={_id}
+                            />
+                            <div className="col-head w-100 d-flex justify-content-between pl-3 pr-3   ">
+                                <h5>{columnItem.columnName}</h5>
+                                <Tooltip title="Add task">
+                                    <Button
+                                        icon={<FileAddOutlined />}
+                                        shape="circle"
+                                        onClick={() => {
+                                            setModalVisible(true);
+                                            setactiveColumnName(
+                                                columnItem?.columnName
+                                            );
                                         }}
-                                    >
-                                        {columnItem?.tasks.map(
-                                            (task, index) => {
-                                                return (
-                                                    <Draggable
-                                                        // isDragDisabled={
-                                                        //     true
-                                                        // }
-                                                        draggableId={
-                                                            "draggable" +
-                                                            columnItem.columnName +
-                                                            index
-                                                        }
-                                                        key={index}
-                                                        index={index}
-                                                    >
-                                                        {(
-                                                            providedD,
-                                                            snapshotD
-                                                        ) => {
-                                                            return (
-                                                                <div
-                                                                    ref={
-                                                                        providedD.innerRef
-                                                                    }
-                                                                    {...providedD.draggableProps}
-                                                                    {...providedD.dragHandleProps}
-                                                                    style={{
-                                                                        userSelect:
-                                                                            "none",
-                                                                        ...providedD
-                                                                            .draggableProps
-                                                                            .style,
-                                                                    }}
-                                                                >
-                                                                    <TaskCard
-                                                                        title={
-                                                                            task.title
+                                    />
+                                </Tooltip>
+                            </div>
+                            <Droppable
+                                droppableId={columnItem.columnName}
+                                key={i}
+                            >
+                                {(provided, snapshot) => {
+                                    return (
+                                        <div
+                                            className="colmn-cont-secondary"
+                                            ref={provided.innerRef}
+                                            {...provided.droppableProps}
+                                            style={{
+                                                minHeight: "100px",
+
+                                                background: snapshot.isDraggingOver
+                                                    ? "lightblue"
+                                                    : "transparent",
+                                            }}
+                                        >
+                                            {columnItem?.tasks.map(
+                                                (task, index) => {
+                                                    return (
+                                                        <Draggable
+                                                            // isDragDisabled={
+                                                            //     true
+                                                            // }
+                                                            draggableId={
+                                                                "draggable" +
+                                                                columnItem.columnName +
+                                                                index
+                                                            }
+                                                            key={index}
+                                                            index={index}
+                                                        >
+                                                            {(
+                                                                providedD,
+                                                                snapshotD
+                                                            ) => {
+                                                                return (
+                                                                    <div
+                                                                        ref={
+                                                                            providedD.innerRef
                                                                         }
-                                                                        label={
-                                                                            task.label
-                                                                        }
-                                                                    />
-                                                                </div>
-                                                            );
-                                                        }}
-                                                    </Draggable>
-                                                );
-                                            }
-                                        )}
-                                        {provided.placeholder}
-                                    </div>
-                                );
-                            }}
-                        </Droppable>
-                    </div>
-                );
-            })}
-        </DragDropContext>
+                                                                        {...providedD.draggableProps}
+                                                                        {...providedD.dragHandleProps}
+                                                                        style={{
+                                                                            userSelect:
+                                                                                "none",
+                                                                            ...providedD
+                                                                                .draggableProps
+                                                                                .style,
+                                                                        }}
+                                                                    >
+                                                                        <TaskCard
+                                                                            title={
+                                                                                task.title
+                                                                            }
+                                                                            label={
+                                                                                task.label
+                                                                            }
+                                                                        />
+                                                                    </div>
+                                                                );
+                                                            }}
+                                                        </Draggable>
+                                                    );
+                                                }
+                                            )}
+                                            {provided.placeholder}
+                                        </div>
+                                    );
+                                }}
+                            </Droppable>
+                        </div>
+                    );
+                })}
+            </DragDropContext>
+        </>
     );
 }
 const mapStateToProps = ({board}) => ({
---
+++ client/src/Components/Board/TaskCard/AddTaskModal.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Modal from "antd/lib/modal/Modal";
+import {Button, Divider} from "antd";
+import {Input} from "antd";
+import {notification} from "antd";
+import { addTask } from "../../../Utils/boardHelpers";
+
+function AddTaskModal({modalVisible, onCancel,_id,columnName}) {
+    console.log(modalVisible, onCancel, _id, columnName);
+    const handleOk = e => {
+        e.preventDefault();
+        const data = {
+            title: e.target.title.value,
+            label: e.target.label.value,
+            description: e.target.description.value,
+            _id,
+            columnName,
+        };
+        addTask(data,(d)=>{
+            console.log(d);
+            onCancel();
+        })
+        console.log(data);
+    };
+    return (
+        <div>
+            <Modal
+                visible={modalVisible}
+                title="Title"
+                onOk={handleOk}
+                onCancel={onCancel}
+                footer={null}
+            >
+                <form onSubmit={handleOk}>
+                    <div>
+                        <label htmlFor="title">Enter Title of Task</label>
+                        <Input type="text" name="title" required />
+                    </div>{" "}
+                    <div>
+                        <label htmlFor="label">Add Label</label>
+                        <Input type="text" name="label" required />
+                    </div>{" "}
+                    <div>
+                        <label htmlFor="description">Description </label>
+                        <Input type="text" name="description" required />
+                    </div>
+                    <br />
+                    <button
+                        key="submit"
+                        className="btn-sm btn mr-3 bg-aqua  border"
+                        type="button"
+                        onClick={onCancel}
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        key="submit"
+                        className="btn-sm btn ml-3 bg-primary text-white"
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </Modal>
+        </div>
+    );
+}
+
+export default AddTaskModal;
