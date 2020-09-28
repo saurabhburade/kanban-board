b87d67c80ea1d8806cc6a9b6356cdeec9397b52c
Saurabh Burade <sburade3@gmail.com>
2020-09-28 22:19:29 +0530

    Modified hero section and action btns

--- client/src/Components/home/Home.js
+++ client/src/Components/home/Home.js
@@ -13,6 +13,8 @@ import mongoIcon from "../../assets/mongo.svg";
 import bootstrapIcon from "../../assets/bootstrap.svg";
 import antIcon from "../../assets/ant.svg";
 import firebaseIcon from "../../assets/firebase.svg";
+import scrumBoard from "../../assets/undraw_Scrum_board_re_wk7v.svg";
+import check from "../../assets/undraw_Booked_re_vtod.svg";
 import {GithubOutlined} from "@ant-design/icons";
 import "animate.css";
 import WOW from "wowjs";
@@ -24,21 +26,38 @@ function Home() {
             <div className="hero-container ">
                 <div className="hero-text wow animate__animated animate__fadeInUp   ">
                     <p>
-                        Kanban Board</p>
-                    <p>
-                        Control, Collaborate & Structure your project through Kanban Boards. 
+                        <p
+                            className="p-0 m-0 text-muted"
+                            style={{fontSize: "0.4em"}}
+                        >
+                            {" "}
+                            Visualize project worlflows with
+                        </p>{" "}
+                        Kanban Board
                     </p>
-                    <div className="action-btns d-flex mt-3">
+                    <p>
+                        Kanban Board is most preffered way to manage project
+                        workflows for Agile Teams.
+                    </p>
+                    <div className="w-100 action-btns d-flex mt-3">
                         {isAuth() ? null : (
                             <>
                                 <Link to="/login">
                                     {" "}
-                                    <AntButton size="sm p-2 pl-5 pr-5">
+                                    <AntButton
+                                        size="sm"
+                                        type="primary"
+                                        className=" p-3 pl-5 pr-5"
+                                    >
                                         Login
                                     </AntButton>
                                 </Link>
                                 <Link to="/register">
-                                    <AntButton size="sm p-2 ml-5 pl-5 pr-5">
+                                    <AntButton
+                                        size="sm"
+                                        type="primary"
+                                        className=" p-3 ml-5 pl-5 pr-5"
+                                    >
                                         Sign Up
                                     </AntButton>
                                 </Link>
@@ -48,9 +67,29 @@ function Home() {
                 </div>
                 <div className="hero-image-cont wow animate__animated animate__fadeInUp">
                     <img className="hero-image" src={image} alt="" />
-                </div>      
+                </div>
             </div>
-            <div className="steps">
+            <div className="defination mt-5 mb-5">
+                <blockquote class="blockquote text-center">
+                    <p class="blockquote-header" style={{fontSize: "0.9em"}}>
+                        What is kanban?
+                    </p>
+                    <p class="mb-0" style={{fontSize: "0.8em"}}>
+                        Kanban is a method for managing the creation of products
+                        with an emphasis on continual delivery while not
+                        overburdening the development team. Like Scrum, Kanban
+                        is a process designed to help teams work together more
+                        effectively.
+                    </p>
+                    <footer
+                        class="blockquote-footer "
+                        style={{fontSize: "0.7em"}}
+                    >
+                        Digital.ai
+                    </footer>
+                </blockquote>
+            </div>
+            <div className="steps mt-5">
                 <h3>Steps</h3>
                 <div className="d-flex align-items-center wow animate__animated animate__fadeInUp">
                     <Steps progressDot direction="vertical">
@@ -102,7 +141,31 @@ function Home() {
                         />
                     </div>
                 </div>
-            </div>{" "}
+            </div>
+
+            <div className=" upFoot-cont d-flex ">
+                <img src={scrumBoard} className="scrumImg" alt="scrumBoard" />
+                <div className="d-flex flex-column align-items-center">
+                    <p style={{fontSize: "1.4em", fontWeight: 600}}>
+                        Start Structuring Your Project Workflows Today{" "}
+                    </p>
+                    <p>Create account now and manage your project workflow.</p>
+                    {isAuth() ? null : (
+                        <>
+                            <Link to="/login">
+                                <AntButton
+                                    size="sm"
+                                    type="primary"
+                                    className=" p-3 pl-5 pr-5"
+                                >
+                                    Lets Get Started
+                                </AntButton>
+                            </Link>
+                        </>
+                    )}
+                </div>
+                <img src={check} className="scrumImg" alt="scrumBoard" />
+            </div>
             <div className="steps tech-cont">
                 <h3>Technologies Used</h3>
                 <div className="d-flex align-items-center justify-content-center flex-wrap wow  animate__animated animate__fadeInUpwow  animate__animated animate__fadeInUp">
@@ -147,7 +210,8 @@ function Home() {
                         </div>
                     </Tooltip>
                 </div>
-            </div>{" "}
+            </div>
+
             <div className="steps tech-cont ">
                 <h3 className="pb-0 wow  animate__animated animate__fadeInUp">
                     About Project
