19b9751ad17e4f9c2bc5750d6062c938738d917f
Saurabh Burade <sburade3@gmail.com>
2020-09-30 21:54:42 +0530

    Fix misstatement of username and initials

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -11,20 +11,26 @@ import MenuItem from "./MenuItem";
 import {connect} from "react-redux";
 import BoardCard from "./BoardCard";
 import {Button} from "antd";
-import UserProfile from './../UserProfile/UserProfile';
-import AddBoardModal from './../AddBoard/AddBoardModal';
+import UserProfile from "./../UserProfile/UserProfile";
+import AddBoardModal from "./../AddBoard/AddBoardModal";
 function Profile({user}) {
     const [dashView, setDashView] = useState(0);
-    const [modalVisible, setModalVisible] = useState(false)
-    const handleModalCancel=()=>{
-        setModalVisible(false)
-    }
+    const [modalVisible, setModalVisible] = useState(false);
+    const handleModalCancel = () => {
+        setModalVisible(false);
+    };
     return (
         <div className="profile-dash-cont">
             <div className="dash-menu-cont">
                 <div className="user">
-                    <p>SB</p>
-                    <p>Saurabh Burade</p>
+                    <p>
+                        {user?.fname ? user?.fname[0] + user?.lname[0] : ".."}
+                    </p>
+                    <p>
+                        {user?.fname
+                            ? user?.fname + " " + user?.lname
+                            : "Loading..."}
+                    </p>
                 </div>
                 <div>
                     <div onClick={() => setDashView(0)}>
@@ -54,7 +60,7 @@ function Profile({user}) {
                             icon={<AppstoreAddOutlined />}
                             size={"middle"}
                             className="ml-5"
-                            onClick={()=>setModalVisible(true)}
+                            onClick={() => setModalVisible(true)}
                         >
                             Add New Board
                         </Button>
@@ -71,7 +77,17 @@ function Profile({user}) {
                     </div>
                 </div>
             ) : dashView === 1 ? (
-                <UserProfile />
+                <UserProfile
+                    name={
+                        user?.fname
+                            ? user?.fname + " " + user?.lname
+                            : "Loading..."
+                    }
+                    email={user?.email ? user.email : "Loading..."}
+                    initial={
+                        user?.fname ? user?.fname[0] + user?.lname[0] : ".."
+                    }
+                />
             ) : (
                 <div className="boards-main-cont">
                     <Button
@@ -84,25 +100,12 @@ function Profile({user}) {
                     </Button>
                 </div>
             )}
-            {/* <div className="boards-main-cont">
-                <div className="board-title-add-board mt-4">
-                    <p>Your Boards</p>{" "}
-                    <Button
-                        shape="round"
-                        icon={<AppstoreAddOutlined />}
-                        size={"middle"}
-                        className="ml-5"
-                    >
-                        Add New Board
-                    </Button>
-                </div>
-                <div className="boards-cont">
-                    {user?.boards?.map((element, index) => {
-                        return <BoardCard name={element.title} />;
-                    })}
-                </div>
-            </div> */}
-            <AddBoardModal  modalVisible={modalVisible} owner={user?.email} onCancel={handleModalCancel}/>
+
+            <AddBoardModal
+                modalVisible={modalVisible}
+                owner={user?.email}
+                onCancel={handleModalCancel}
+            />
         </div>
     );
 }
