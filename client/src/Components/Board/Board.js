8594aaddc39c7b29c18756b20b5dc8ef412c1083
Saurabh Burade <sburade3@gmail.com>
2020-10-11 13:44:50 +0530

    Feat : Add Board team badge

--- client/src/Components/Board/Board.js
+++ client/src/Components/Board/Board.js
@@ -5,7 +5,8 @@ import {connect} from "react-redux";
 import {fetchBoard} from "./../../Redux/board/board.actionCreators";
 import Column from "./Column/Column";
 import {socket} from "./../../Utils/socket";
-
+import Badge from "../Presentational/Badge/Badge";
+import {MergeCellsOutlined} from "@ant-design/icons";
 function Board(props) {
     useEffect(() => {
         console.log("props", props);
@@ -25,8 +26,19 @@ function Board(props) {
     }, []);
     return (
         <div className="board-cont-main">
-            <div className="board-head">
-                <h4>{props?.board?.title}</h4>
+            <div className="board-head d-flex justify-content-start align-items-center ml-3 mr-3">
+                <h4 className="p-0 m-0">{props?.board?.title}</h4>
+                <div className="d-flex m-0 ml-3 mr-3">
+                    {props.board?.team ? (
+                        <Badge
+                            title={props.board?.owner}
+                            icon={<MergeCellsOutlined />}
+                        />
+                    ) : null}
+                    {props.board?.team?.map(element => {
+                        return <Badge title={element} />;
+                    })}
+                </div>
             </div>
             <div className="columns-cont-main d-flex">
                 <Column _id={props?.match?.params?.id} />
