3c13f4e5d6fbc2b93d53068f7fbada1b0ed34bef
Saurabh Burade <sburade3@gmail.com>
2020-11-01 11:15:15 +0530

    Add Lodable App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import {Provider} from "react-redux";
 import {store} from "./Redux";
+import Loadable from "react-loadable";
+import {Spin} from "antd";
+import {LoadingOutlined} from "@ant-design/icons";
 
+const antIcon = <LoadingOutlined style={{fontSize: 24}} spin />;
+ const App = Loadable({
+    loader: () => import("./App"),
+    loading: () => <Spin indicator={antIcon} />,
+});
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
